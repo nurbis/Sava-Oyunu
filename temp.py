47b661d1334b2ab293263aaf90324a7ad7a6779a
nurbis <48757526+nurbis@users.noreply.github.com>
2020-03-19 23:24:59 +0300

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,25 +1,80 @@
-from __future__ import absolute_import, division, print_function
-from toolz import memoize
-from .drop import drop
-from .utils import copydoc
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from random import randint
+class Silah:
+    def __init__(self, isim:str, damage:int ):
+        self.__isim=isim
+        self.__damage=damage
+        
+    def vur(self, rakip):
+        rakip.setCan(rakip.getCan()-self.__damage)
+        self.__damage-=1
+    def getIsim(self):
+        return self.__isim
+    def getDamage(self):
+        return self.__damage
+    
+    
+class Karakter:
+    def __init__(self, can:int, silah:Silah):
+        self.__can=can
+        self.__silah=silah
+    def getCan(self):
+        return self.__can
+    def setCan(self, yenican):
+        self.__can=yenican
+    def vur(self,rakip):
+        self.__silah.vur(rakip)
+    def getSilahIsim(self):
+        return self.__silah.getIsim()
+    def getDamage(self):
+        return self.__silah.getDamage()
+    
+    
+class Dusman(Karakter):
+    def __init__(self, can:int, silah:Silah):
+        super().__init__(can, silah)
+        
+class Oyuncu(Karakter):
+    def __init__(self, can:int, silah:Silah, isim:str):
+        super().__init__(can, silah)
+        self.__isim=isim
+    def getIsim(self):
+        return self.__isim
 
 
-class _Temp(object):
-    """ Temporary version of persistent storage
+      
+dusmanlar = list()
+for i in range(10):
+    dusmanlar.append(Dusman(randint(30,50),Silah("Bıçak", randint(10,15))))
+         
+NurHERO=Oyuncu(140, Silah("Kılıç",35), "Nur")   
+      
+    
+while True:
+    print("Oyuncu: {} --- Can: {} --- Silah: {} --- Damage:{}".format(NurHERO.getIsim(), NurHERO.getCan(), NurHERO.getSilahIsim(), NurHERO.getDamage()))
+    print("=======================================================================")
+    for sayi, i in enumerate(dusmanlar):
+        print("Dusman: {} --- Can: {} --- Silah: {} --- Damage:{}".format(sayi, i.getCan(), i.getSilahIsim(), i.getDamage()))
+    secim=input("Düşman Seçiniz")
+    vurulandusman=dusmanlar[int(secim)]
+    NurHERO.vur(vurulandusman)
+    if vurulandusman.getCan() <= 0:
+        dusmanlar.remove(vurulandusman)
+        if not dusmanlar:
+            print("Kazandın")
+            break
+        if dusmanlar:
+            dusmanlar[randint(0, len(dusmanlar)-1)].vur(NurHERO)
+            if NurHERO.getCan() <= 0:
+                print("Kaybettin")
+                break
 
-    Calls ``drop`` on object at garbage collection
-
-    This is a parametrized type, so call it on types to make new types
-
-    >>> from odo import Temp, CSV
-    >>> csv = Temp(CSV)('/tmp/myfile.csv', delimiter=',')
-    """
-    def __del__(self):
-        drop(self)
-
-
-@memoize
-@copydoc(_Temp)
-def Temp(cls):
-    """ Parametrized Chunks Class """
-    return type('Temp(%s)' % cls.__name__, (_Temp, cls), {'persistent_type': cls})
+         
+         
+     
+     
